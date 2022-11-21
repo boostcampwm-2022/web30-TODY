e1a134cdc86e18e1fc43afe2da816f700054b5e9
boostksm <boostksm@gmail.com>
2022-11-21 22:11:40 +0900

    #17 feat: 회원가입, 닉네임+아이디 중복 체크 api 생성

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,13 +1,71 @@
-import { Controller, Get } from '@nestjs/common';
-import { User } from './entities/user.entity';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
-  @Get()
-  findAll(): Promise<User[]> {
-    return this.userService.findAll();
+  @Post('signup')
+  @HttpCode(200)
+  async signup(
+    @Body() userInfo: { id: string; password: string; nickname: string },
+  ): Promise<{ nickname: string }> {
+    try {
+      const { nickname } = await this.userService.createUser(userInfo);
+      return { nickname };
+    } catch (err) {
+      throw new HttpException(
+        {
+          status: HttpStatus.BAD_REQUEST,
+          error: 'bad request',
+        },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+  }
+
+  @Get('checkID')
+  @HttpCode(200)
+  async findOneById(@Query('id') id: string): Promise<{ isUnique: boolean }> {
+    try {
+      const checkId = await this.userService.findOneById(id);
+      return checkId ? { isUnique: true } : { isUnique: false };
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.FORBIDDEN,
+          error: 'Unexpected error',
+        },
+        HttpStatus.FORBIDDEN,
+      );
+    }
+  }
+
+  @Get('checkNickname')
+  @HttpCode(200)
+  async findOneByNickname(
+    @Query('nickname') nickname: string,
+  ): Promise<{ isUnique: boolean }> {
+    try {
+      const checkNickname = await this.userService.findOneByNickname(nickname);
+      return checkNickname ? { isUnique: true } : { isUnique: false };
+    } catch (error) {
+      throw new HttpException(
+        {
+          status: HttpStatus.FORBIDDEN,
+          error: 'Unexpected error',
+        },
+        HttpStatus.FORBIDDEN,
+      );
+    }
   }
 }
--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { User } from '../user/entities/user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { getSalt, getSecurePassword } from 'src/utils/salt';
 
 @Injectable()
 export class UserService {
@@ -10,7 +11,30 @@ export class UserService {
     private userRepository: Repository<User>,
   ) {}
 
-  async findAll(): Promise<User[]> {
-    return this.userRepository.find();
+  async createUser(userInfo: {
+    id: string;
+    password: string;
+    nickname: string;
+  }): Promise<{ nickname: string }> {
+    const salt = await getSalt();
+    const securePassword = await getSecurePassword(userInfo.password, salt);
+    const newUser = await this.userRepository.insert({
+      userId: userInfo.id,
+      userPw: securePassword,
+      nickname: userInfo.nickname,
+      salt,
+    });
+    const insertedId = newUser.identifiers[0].userId;
+    const user = await this.userRepository.findOne({
+      where: { userId: insertedId },
+    });
+    return { nickname: user.nickname };
+  }
+
+  async findOneById(id: string): Promise<User> {
+    return this.userRepository.findOne({ where: { userId: id } });
+  }
+  async findOneByNickname(nickname: string): Promise<User> {
+    return this.userRepository.findOne({ where: { nickname: nickname } });
   }
 }
