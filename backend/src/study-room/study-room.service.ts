4285aa6aa1687c4e6531d38e5996ddba60265cee
lass9436 <lass9436@gmail.com>
2022-11-23 01:24:49 +0900

    #18 feat: 공부방 목록 출력 api 작성

--- backend/src/study-room/study-room.service.ts
+++ backend/src/study-room/study-room.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { StudyRoom } from './entities/studyRoom.entity';
-import { Repository } from 'typeorm';
+import { Repository, Like } from 'typeorm';
 import { createRoomDto } from './dto/createRoom.dto';
+import { dateFormatter } from 'src/utils/dateFormatter';
 
 @Injectable()
 export class StudyRoomService {
@@ -12,8 +13,6 @@ export class StudyRoomService {
   ) {}
 
   async createStudyRoom(roomInfo: createRoomDto) {
-    console.log(roomInfo);
-
     await this.studyRoomRepository.insert({
       studyRoomName: roomInfo.name,
       studyRoomContent: roomInfo.content,
@@ -30,12 +29,54 @@ export class StudyRoomService {
     attendable: boolean,
     page: number,
   ) {
-    const studyRoomList = await this.studyRoomRepository.find();
+    if (!keyword) {
+      keyword = '';
+    }
+    const currentPage = page > 0 ? page : 1;
+    const studyRoomListData = await this.studyRoomRepository.find({
+      where: [
+        { studyRoomName: Like('%' + keyword + '%') },
+        { studyRoomContent: Like('%' + keyword + '%') },
+        { tag1: Like('%' + keyword + '%') },
+        { tag2: Like('%' + keyword + '%') },
+      ],
+      order: {
+        createTime: {
+          direction: 'DESC',
+        },
+      },
+      take: 9,
+      skip: 9 * (currentPage - 1),
+    });
+    console.log(studyRoomListData);
+    const totalCount = await this.studyRoomRepository.count();
+    const pageCount = Math.ceil(totalCount / 9);
+    const studyRoomList = studyRoomListData.map((roomInfo) => {
+      const tags = [];
+      if (roomInfo.tag1) {
+        tags.push(roomInfo.tag1);
+      }
+      if (roomInfo.tag2) {
+        tags.push(roomInfo.tag2);
+      }
+
+      const data = {
+        studyRoomId: roomInfo.studyRoomId,
+        name: roomInfo.studyRoomName,
+        content: roomInfo.studyRoomContent,
+        maxPersonnel: roomInfo.maxPersonnel,
+        currentPersonnel: 1,
+        tags,
+        created: dateFormatter(roomInfo.createTime),
+      };
+      return data;
+    });
+
     const searchResult = {
       keyword,
-      currentPage: page,
-      pageCount: 5,
-      totalCount: 45,
+      currentPage,
+      pageCount,
+      totalCount,
       studyRoomList,
     };
     return searchResult;
