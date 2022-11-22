42aec8a240c39cd4832495f3d694dec6b37512bb
boostksm <boostksm@gmail.com>
2022-11-22 13:46:31 +0900

    #17 feat: User DTO 추가 및 global exception filter 설정

--- backend/src/user/user.controller.ts
+++ backend/src/user/user.controller.ts
@@ -1,71 +1,30 @@
-import {
-  Body,
-  Controller,
-  Get,
-  Post,
-  Query,
-  HttpCode,
-  HttpException,
-  HttpStatus,
-} from '@nestjs/common';
+import { Body, Controller, Get, Post, Param } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/user.dto';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
   @Post('signup')
-  @HttpCode(200)
-  async signup(
-    @Body() userInfo: { id: string; password: string; nickname: string },
-  ): Promise<{ nickname: string }> {
-    try {
-      const { nickname } = await this.userService.createUser(userInfo);
-      return { nickname };
-    } catch (err) {
-      throw new HttpException(
-        {
-          status: HttpStatus.BAD_REQUEST,
-          error: 'bad request',
-        },
-        HttpStatus.BAD_REQUEST,
-      );
-    }
+  async signup(@Body() userData: CreateUserDto): Promise<{ nickname: string }> {
+    const { nickname } = await this.userService.createUser(userData);
+    return { nickname };
   }
 
-  @Get('checkID')
-  @HttpCode(200)
-  async findOneById(@Query('id') id: string): Promise<{ isUnique: boolean }> {
-    try {
-      const checkId = await this.userService.findOneById(id);
-      return checkId ? { isUnique: true } : { isUnique: false };
-    } catch (error) {
-      throw new HttpException(
-        {
-          status: HttpStatus.FORBIDDEN,
-          error: 'Unexpected error',
-        },
-        HttpStatus.FORBIDDEN,
-      );
-    }
+  @Get('checkID/:id')
+  async findOneById(@Param('id') id: string): Promise<{ isUnique: boolean }> {
+    const checkId = await this.userService.findOneById({ id });
+    return checkId ? { isUnique: false } : { isUnique: true };
   }
 
-  @Get('checkNickname')
-  @HttpCode(200)
+  @Get('checkNickname/:nickname')
   async findOneByNickname(
-    @Query('nickname') nickname: string,
+    @Param('nickname') nickname: string,
   ): Promise<{ isUnique: boolean }> {
-    try {
-      const checkNickname = await this.userService.findOneByNickname(nickname);
-      return checkNickname ? { isUnique: true } : { isUnique: false };
-    } catch (error) {
-      throw new HttpException(
-        {
-          status: HttpStatus.FORBIDDEN,
-          error: 'Unexpected error',
-        },
-        HttpStatus.FORBIDDEN,
-      );
-    }
+    const checkNickname = await this.userService.findOneByNickname({
+      nickname,
+    });
+    return checkNickname ? { isUnique: false } : { isUnique: true };
   }
 }
