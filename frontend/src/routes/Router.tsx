c01cdebacb8511a95eda4b7d2a5fcc11f3fde972
sieun <sieun0115@ajou.ac.kr>
2022-11-24 19:21:35 +0900

    #18 feat: 홈 페이지 경로 추가 및 사이드 바 메뉴 클릭 시, 페이지 이동

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LoginPage from '@pages/LoginPage';
 import SignupPage from '@pages/SignupPage';
 import StudyRoomListPage from '@pages/StudyRoomListPage';
+import MainPage from '@pages/MainPage';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/study-rooms" element={<StudyRoomListPage />} />
