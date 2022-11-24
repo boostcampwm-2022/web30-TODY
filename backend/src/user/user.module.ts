47d181ac8acbcd90b1b353d14625e106993bfd1a
lass9436 <lass9436@gmail.com>
2022-11-24 22:17:22 +0900

    #18 fix: env 불러오기 수정

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -4,12 +4,11 @@ import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ envFilePath: `.env.${process.env.NODE_ENV}` }),
     TypeOrmModule.forFeature([User]),
     JwtModule.register({ secret: process.env.JWT_SECRET }),
   ],
