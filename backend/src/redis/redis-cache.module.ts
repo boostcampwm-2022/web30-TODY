2d0decfb65ef791110e65702b6ef75e9db7c0fbe
sieun <sieun0115@ajou.ac.kr>
2022-11-24 14:36:41 +0900

    #28 conf: redis cloud 연동

--- backend/src/redis/redis-cache.module.ts
+++ backend/src/redis/redis-cache.module.ts
@@ -2,16 +2,21 @@ import { CacheModule, Module } from '@nestjs/common';
 import * as redisStore from 'cache-manager-ioredis';
 import { RedisCacheController } from './redis-cache.controller';
 import { RedisCacheService } from './redis-cache.service';
-
-const cacheModule = CacheModule.register({
-  store: redisStore,
-  host: 'localhost',
-  port: '6379',
-  ttl: 0,
-});
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [cacheModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    CacheModule.register({
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD,
+      ttl: 0,
+    }),
+  ],
   controllers: [RedisCacheController],
   providers: [RedisCacheService],
   exports: [RedisCacheService],
