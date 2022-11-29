668a52fbac29d4a76033f61780b6dac99cecc8b4
Park HyeonJun <darkeroe@hanyang.ac.kr>
2022-11-29 16:14:39 +0900

    #11 conf: https option 제거
    Co-authored-by: 김시은 <SE0115@users.noreply.github.com>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,11 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 import { AllExceptionsFilter } from './filter/all-exceptions.filter';
 
 async function bootstrap() {
-  const httpsOptions = {
-    key: fs.readFileSync(path.join(__dirname, process.env.KEY_PATH)),
-    cert: fs.readFileSync(path.join(__dirname, process.env.CERT_PATH)),
-  };
-  const app = await NestFactory.create(AppModule, { httpsOptions, cors: true });
+  // const httpsOptions = {
+  //   key: fs.readFileSync(path.join(__dirname, process.env.KEY_PATH)),
+  //   cert: fs.readFileSync(path.join(__dirname, process.env.CERT_PATH)),
+  // };
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
