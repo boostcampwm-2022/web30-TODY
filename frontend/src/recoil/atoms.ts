ffa6fba24dac7dff5855232da25ca88093f0dfa9
boostksm <boostksm@gmail.com>
2022-11-29 18:09:14 +0900

    #35 feat: login api 요청 시 user 정보 저장 및 사이드바에 적용

--- frontend/src/recoil/atoms.ts
+++ frontend/src/recoil/atoms.ts
@@ -1,6 +1,6 @@
 import { atom } from 'recoil';
 
-export const userState = atom({
+export const userState = atom<{ userId: string; nickname: string } | null>({
   key: 'userState',
   default: null,
 });
