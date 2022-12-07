22ae18aaa8c6fbb39fd62f8250ed43718285e682
boostksm <boostksm@gmail.com>
2022-12-07 15:04:07 +0900

    #74 fix: 공부방 페이지 사이드바 렌더링
    Co-authored-by: 김시은 <SE0115@users.noreply.github.com>

--- frontend/src/pages/SfuPage.tsx
+++ frontend/src/pages/SfuPage.tsx
@@ -371,12 +371,12 @@ export default function SfuPage() {
             ))}
           </VideoList>
         </VideoListLayout>
-        {/* {activeSideBar !== '' &&
+        {activeSideBar !== '' &&
           (activeSideBar === '채팅' ? (
             <ChatSideBar />
           ) : (
             <ParticipantsSideBar participants={participantsList} />
-          ))} */}
+          ))}
       </Content>
       <BottomBarLayout>
         <MenuList onClick={onClickButtons}>
