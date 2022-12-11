8da78eff2eae8230597476db8791bcf8b3795940
boostksm <boostksm@gmail.com>
2022-12-11 14:20:14 +0900

    #104 feat: silent-login 후에 라우팅 처리

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,23 +1,37 @@
 import { useSetRecoilState } from 'recoil';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import useAxios from '@hooks/useAxios';
 import { userState } from 'recoil/atoms';
+import Loader from '@components/common/Loader';
 import silentLoginRequest from './axios/requests/silentLoginRequest';
 import Router from './routes/Router';
 
 function App() {
+  const [isAuthDone, setIsAuthDone] = useState(false);
   const [requestSilentLogin, loading, err, silentLoginData] =
     useAxios(silentLoginRequest);
   const setUser = useSetRecoilState(userState);
 
   useEffect(() => {
+    if (!err) return;
+    if (err.status === 401) {
+      setIsAuthDone(true);
+    }
+  }, [err]);
+
+  useEffect(() => {
     if (silentLoginData === null) return;
     setUser(silentLoginData);
+    setIsAuthDone(true);
   }, [setUser, silentLoginData]);
 
   useEffect(() => {
     requestSilentLogin();
   }, [requestSilentLogin]);
+
+  if (!isAuthDone) {
+    return <Loader />;
+  }
 
   return <Router />;
 }
