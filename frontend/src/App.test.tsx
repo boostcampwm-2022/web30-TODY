795f801869a0a38b9bfe15a81757334880b7b9f3
boostksm <boostksm@gmail.com>
2022-12-12 03:10:44 +0900

    #108 test: App 테스트 코드 수정

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,6 +1,11 @@
 import { render } from '@testing-library/react';
+import { RecoilRoot } from 'recoil';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>,
+  );
 });
