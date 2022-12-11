adf1059a73142b1e428aa30483d7b565e66787ee
boostksm <boostksm@gmail.com>
2022-12-11 14:19:39 +0900

    #104 feat: 게스트접근 비허용 페이지에 PrivateRoute 적용

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -6,17 +6,39 @@ import MainPage from '@pages/MainPage';
 import InitPage from '@pages/InitPage';
 import SfuPage from '@pages/SfuPage';
 import MeshPage from '@pages/MeshPage';
+import PrivateRoute from '@components/common/PrivateRoute';
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<InitPage />} />
-        <Route path="/home" element={<MainPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="/study-room/:roomId" element={<SfuPage />} />
-        <Route path="/study-rooms" element={<StudyRoomListPage />} />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <MainPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/study-room/:roomId"
+          element={
+            <PrivateRoute>
+              <SfuPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/study-rooms"
+          element={
+            <PrivateRoute>
+              <StudyRoomListPage />
+            </PrivateRoute>
+          }
+        />
         <Route path="/study-room-mesh" element={<MeshPage />} />
       </Routes>
     </BrowserRouter>
