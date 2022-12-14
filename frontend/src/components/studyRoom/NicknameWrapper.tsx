91be2556737ccdec6ae296662ca93d1c78ebf0ac
boostksm <boostksm@gmail.com>
2022-12-14 21:14:37 +0900

    #139 feat:  닉네임 표시 컴포넌트 정의
    Co-authored-by: 김시은 <SE0115@users.noreply.github.com>

---
+++ frontend/src/components/studyRoom/NicknameWrapper.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+const NicknameWrapperLayout = styled.div`
+  position: relative;
+  /* width: 100%; */
+`;
+
+const NameBox = styled.div`
+  position: absolute;
+  top: 15px;
+  left: 15px;
+  padding: 6px 10px;
+  border-radius: 5px;
+  background: rgba(37, 37, 37, 0.39);
+  color: white;
+`;
+
+export default function NicknameWrapper({ children, nickname }: any) {
+  return (
+    <NicknameWrapperLayout>
+      <NameBox>{nickname}</NameBox>
+      {children}
+    </NicknameWrapperLayout>
+  );
+}
