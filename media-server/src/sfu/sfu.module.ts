8f3ab182b49591133dad9dfa7e9f9680d003194b
boostksm <boostksm@gmail.com>
2022-12-16 03:01:31 +0900

    hotfix: 미디어서버 NestJS 환경변수 설정

--- media-server/src/sfu/sfu.module.ts
+++ media-server/src/sfu/sfu.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SfuGateway } from './sfu.gateway';
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV}`,
+      isGlobal: true,
+    }),
+  ],
   providers: [SfuGateway],
 })
 export class SfuModule {}
