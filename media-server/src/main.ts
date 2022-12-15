83759401b877567c59b6d40227a685563553dd37
lass9436 <lass9436@gmail.com>
2022-12-15 13:43:33 +0900

    #135 fix: redis module 분리 및 미디어 서버 분리 작업
    Co-authored-by: Darkeroe <myhjun@naver.com>

--- media-server/src/main.ts
+++ media-server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
-import { MediaServerModule } from './mediaServer/mediaServer.module';
+//import { MediaServerModule } from './mediaServer/mediaServer.module';
+import { SfuModule } from './sfu/sfu.module';
 
 async function bootstrap() {
   const port = 9000;
   console.log(`Server is running on port ${port}`);
-  const mediaServerApp = await NestFactory.create(MediaServerModule);
+  //const mediaServerApp = await NestFactory.create(MediaServerModule);
+  const mediaServerApp = await NestFactory.create(SfuModule);
   await mediaServerApp.listen(port);
 }
 bootstrap();
