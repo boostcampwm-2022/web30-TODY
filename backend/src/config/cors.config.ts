25637b24cffe6f6b4dbd719ff721c0a106d6fc4b
lass9436 <lass9436@gmail.com>
2022-12-16 10:47:43 +0900

    hotfix: cors-config 수정

--- backend/src/config/cors.config.ts
+++ backend/src/config/cors.config.ts
@@ -1,6 +1,6 @@
 const origin =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://tody.kr';
+    : ['https://tody.kr', 'https://j221-test.tk'];
 
 export default { credentials: true, origin };
