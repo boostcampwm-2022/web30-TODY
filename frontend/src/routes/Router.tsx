3cecb67371c1157c62fbadad1316d35057cc7cb9
boostksm <boostksm@gmail.com>
2022-12-18 06:19:57 +0900

    #203 refactor: React.lazy를 통해 코드 스플리팅 적용

--- frontend/src/routes/Router.tsx
+++ frontend/src/routes/Router.tsx
@@ -1,51 +1,54 @@
 import { Route, Routes } from 'react-router-dom';
-import LoginPage from '@pages/LoginPage';
-import SignupPage from '@pages/SignupPage';
-import StudyRoomListPage from '@pages/StudyRoomListPage';
-import MainPage from '@pages/MainPage';
-import InitPage from '@pages/InitPage';
-import SfuPage from '@pages/SfuPage';
-import MeshPage from '@pages/MeshPage';
-import PrivateRoute from '@components/common/PrivateRoute';
-import StudyRoomGuard from '@components/common/StudyRoomGuard';
-import NotFoundPage from '@pages/NotFoundPage';
-import ErrorPage from '@pages/ErrorPage';
+import { lazy, Suspense } from 'react';
+import Loader from '@components/common/Loader';
+
+const InitPage = lazy(() => import('@pages/InitPage'));
+const SfuPage = lazy(() => import('@pages/SfuPage'));
+const LoginPage = lazy(() => import('@pages/LoginPage'));
+const SignupPage = lazy(() => import('@pages/SignupPage'));
+const StudyRoomListPage = lazy(() => import('@pages/StudyRoomListPage'));
+const MainPage = lazy(() => import('@pages/MainPage'));
+const PrivateRoute = lazy(() => import('@components/common/PrivateRoute'));
+const StudyRoomGuard = lazy(() => import('@components/common/StudyRoomGuard'));
+const NotFoundPage = lazy(() => import('@pages/NotFoundPage'));
+const ErrorPage = lazy(() => import('@pages/ErrorPage'));
 
 export default function Router() {
   return (
-    <Routes>
-      <Route path="/" element={<InitPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route
-        path="/home"
-        element={
-          <PrivateRoute>
-            <MainPage />
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/study-room/:roomId"
-        element={
-          <PrivateRoute>
-            <StudyRoomGuard>
-              <SfuPage />
-            </StudyRoomGuard>
-          </PrivateRoute>
-        }
-      />
-      <Route
-        path="/study-rooms"
-        element={
-          <PrivateRoute>
-            <StudyRoomListPage />
-          </PrivateRoute>
-        }
-      />
-      <Route path="/study-room-mesh" element={<MeshPage />} />
-      <Route path="/error" element={<ErrorPage />} />
-      <Route path="/*" element={<NotFoundPage />} />
-    </Routes>
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path="/" element={<InitPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route
+          path="/home"
+          element={
+            <PrivateRoute>
+              <MainPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/study-room/:roomId"
+          element={
+            <PrivateRoute>
+              <StudyRoomGuard>
+                <SfuPage />
+              </StudyRoomGuard>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/study-rooms"
+          element={
+            <PrivateRoute>
+              <StudyRoomListPage />
+            </PrivateRoute>
+          }
+        />
+        <Route path="/error" element={<ErrorPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
